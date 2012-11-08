45ca0fa6af86f2a467de6a4530f8b38e3ceb6bbd
Niels Falk <niels@elbtrial.com>
2012-11-08 14:54:09 +0100

    test Gravatar Api

--- src/main/java/de/hh/changeRing/UserSession.java
+++ src/main/java/de/hh/changeRing/UserSession.java
@@ -3,6 +3,9 @@ package de.hh.changeRing;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import de.bripkens.gravatar.DefaultImage;
+import de.bripkens.gravatar.Gravatar;
+import de.bripkens.gravatar.Rating;
 
 /**
  * User: nielsfalk Date: 08.11.12 10:18
@@ -14,6 +17,11 @@ public class UserSession {
 
 	public String getStuff() {
 		return "stuff";
+	}
+
+	public String getGravatarUrl() {
+		return new Gravatar().setSize(80).setHttps(true).setRating(Rating.PARENTAL_GUIDANCE_SUGGESTED)
+				.setStandardDefaultImage(DefaultImage.MONSTER).getUrl("niles@elbtrial.com");
 	}
 
 	public String getUserName() {

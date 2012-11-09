2b250cdd9a903d436c7cc5ddcceec534d8226668
Niels Falk <niels@elbtrial.com>
2012-11-09 19:47:12 +0100

    menu & styling

--- src/main/java/de/hh/changeRing/UserSession.java
+++ src/main/java/de/hh/changeRing/UserSession.java
@@ -1,11 +1,12 @@
 package de.hh.changeRing;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
 import de.bripkens.gravatar.DefaultImage;
 import de.bripkens.gravatar.Gravatar;
 import de.bripkens.gravatar.Rating;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 
 /**
  * User: nielsfalk Date: 08.11.12 10:18
@@ -13,26 +14,32 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class UserSession {
-    private String id, password;
+	private String id, password;
 
-    public String getGravatarUrl() {
-        return new Gravatar().setSize(80).setHttps(true).setRating(Rating.PARENTAL_GUIDANCE_SUGGESTED)
-                .setStandardDefaultImage(DefaultImage.MONSTER).getUrl("niles@elbtrial.com");
-    }
+	public String getGravatarUrl() {
+		return new Gravatar().setSize(80).setHttps(true).setRating(Rating.PARENTAL_GUIDANCE_SUGGESTED)
+				.setStandardDefaultImage(DefaultImage.MONSTER).getUrl("niles@elbtrial.com");
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void login() {
-        System.out.println(id);
-    }
+	public void login() {
+		System.out.println(id);
+	}
+
+	public String activeMenu(String viewIdPrefix) {
+		return FacesContext.getCurrentInstance().getViewRoot().getViewId().substring(1).startsWith(viewIdPrefix) ?
+				"activeMenuItem"
+				: "";
+	}
 }

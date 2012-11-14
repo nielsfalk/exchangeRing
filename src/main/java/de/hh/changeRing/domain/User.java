5c77025d05b690405746fa54ab7b423f811c51b4
Niels Falk <niels@elbtrial.com>
2012-11-14 18:15:14 +0100

    added licence header

--- src/main/java/de/hh/changeRing/domain/User.java
+++ src/main/java/de/hh/changeRing/domain/User.java
@@ -17,7 +17,26 @@ import static de.hh.changeRing.domain.User.DepotItemType.in;
 import static de.hh.changeRing.domain.User.DepotItemType.out;
 
 /**
- * User: nielsfalk Date: 08.11.12 17:16
+ * ----------------GNU General Public License--------------------------------
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ----------------in addition-----------------------------------------------
+ *
+ * In addition, each military use, and the use for interest profit will be
+ * excluded.
+ * Environmental damage caused by the use must be kept as small as possible.
  */
 
 @XmlAccessorType(XmlAccessType.PROPERTY)

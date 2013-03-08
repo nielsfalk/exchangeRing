53c01590b3d7a786eb89a3290e2442e1bdf16f7b
Niels Falk <niels@elbtrial.com>
2013-03-08 11:50:28 +0100

    reduced InitTestDataTest

--- src/test/java/de/hh/changeRing/InitTestDataTest.java
+++ src/test/java/de/hh/changeRing/InitTestDataTest.java
@@ -4,7 +4,6 @@ import de.hh.changeRing.transaction.Transaction;
 import de.hh.changeRing.user.User;
 import org.junit.Test;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
@@ -39,46 +38,6 @@ public class InitTestDataTest {
     protected static final List<User> users = InitTestData.getUsers();
     static final List<Transaction> transactions = InitTestData.getTransactions();
     private static final SimpleDateFormat FORMAT = new SimpleDateFormat("yyyy.MM.dd");
-
-    @Test
-    public void loadUsers() {
-        User niels = users.get(0);
-        assertThat(niels.getNickName(), is("niles"));
-        assertThat(niels.getId(), is(577L));
-        assertThat(niels.getFirstName(), is("Niels"));
-        assertThat(niels.isFirstNameVisible(), is(true));
-        assertThat(niels.getLastName(), is("Falk"));
-        assertThat(niels.isLastNameVisible(), is(false));
-        assertThat(niels.getPassword(), is("123"));
-        assertThat(niels.getEmail(), is("niles@elbtrial.com"));
-    }
-
-    @Test
-    public void findUser() {
-        assertThat(InitTestData.findUser("577"), is(users.get(0)));
-        assertThat(InitTestData.findUser("blau"), is(users.get(1)));
-        assertThat(InitTestData.findUser("niels.falk@blau.de"), is(users.get(2)));
-    }
-
-    @Test
-    public void transaction() throws ParseException {
-        Transaction transaction = transactions.get(0);
-        assertThat(transaction.getFrom(), is(InitTestData.findUser(577l)));
-        assertThat(transaction.getTo(), is(InitTestData.findUser(13l)));
-        assertThat(transaction.getAmount(), is(1l));
-        assertThat(transaction.getDate(), is(FORMAT.parse("2012.11.07")));
-    }
-
-    @Test
-    public void transactions() {
-        User niles = InitTestData.findUser(577l);
-        long nilesInitialBalance = niles.getBalance();
-        User blau = InitTestData.findUser(13l);
-        long blauInitialBalance = blau.getBalance();
-        InitTestData.process(Transaction.create(niles, blau, 5l, null));
-        assertThat(niles.getBalance() - nilesInitialBalance, is(-5l));
-        assertThat(blau.getBalance() - blauInitialBalance, is(5l));
-    }
 
     @Test
     public void advertisements() {

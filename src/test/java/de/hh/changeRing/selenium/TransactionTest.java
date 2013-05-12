13841136e5c690361da9af4dd66215fd432c5fa6
Niels Falk <niels@elbtrial.com>
2013-05-12 20:53:12 +0200

    expect expectTransactionLog with transaction Log

--- src/test/java/de/hh/changeRing/selenium/TransactionTest.java
+++ src/test/java/de/hh/changeRing/selenium/TransactionTest.java
@@ -1,6 +1,7 @@
 package de.hh.changeRing.selenium;
 
 import de.hh.changeRing.transaction.TransactionCreator;
+import de.hh.changeRing.user.DepotItemType;
 import de.hh.changeRing.user.User;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -14,9 +15,14 @@ import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
 
 import static de.hh.changeRing.TestUtils.PASSWORD;
 import static de.hh.changeRing.TestUtils.createTestMember;
+import static de.hh.changeRing.user.DepotItemType.in;
+import static de.hh.changeRing.user.DepotItemType.out;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * ----------------GNU General Public License--------------------------------
@@ -39,8 +45,11 @@ import static de.hh.changeRing.TestUtils.createTestMember;
 @RunWith(Arquillian.class)
 @RunAsClient
 public class TransactionTest extends SeleniumTest {
-    private static final User USER = createTestMember();
-    private static final User RECEIVER = createTestMember();
+    private static final BigDecimal INITIAL_BALANCE = new BigDecimal("1000.00");
+    private static final User USER = createTestMember(INITIAL_BALANCE);
+    private static final User RECEIVER = createTestMember(INITIAL_BALANCE);
+    private static final BigDecimal TRANSACTION_AMOUNT = new BigDecimal("15.00");
+    private static final String SUBJECT = "Vielen Dank für die gute Arbeit";
 
 
     @Deployment
@@ -49,19 +58,38 @@ public class TransactionTest extends SeleniumTest {
     }
 
     @Test
-    public void userAuthentication() {
+    public void transaction() {
         login(USER.getEmail(), PASSWORD);
-        transaction();
+        doTransaction();
+        headerContainsNewAmount(INITIAL_BALANCE.subtract(TRANSACTION_AMOUNT));
+        expectTransactionLog(out, TRANSACTION_AMOUNT.negate(), INITIAL_BALANCE.subtract(TRANSACTION_AMOUNT));
+
         logout();
+        login(RECEIVER.getEmail(), PASSWORD);
+        headerContainsNewAmount(INITIAL_BALANCE.add(TRANSACTION_AMOUNT));
+        expectTransactionLog(in, TRANSACTION_AMOUNT, INITIAL_BALANCE.add(TRANSACTION_AMOUNT));
     }
 
-    private void transaction() {
+    private void expectTransactionLog(DepotItemType depotItemType, BigDecimal amount, BigDecimal balance) {
+        browser.open(deploymentUrl.toString() + "/internal/account.xhtml");
+        assertThat(browser.getText("xpath=//*[@id=\"transactionTable_data\"]/tr[1]/td[4]/div"), is(depotItemType.toString()));
+        assertThat(browser.getText("xpath=//*[@id=\"transactionTable_data\"]/tr[1]/td[5]/div"), is(SUBJECT));
+        assertThat(browser.getText("xpath=//*[@id=\"transactionTable_data\"]/tr[1]/td[6]/div"), is(amount.toString()));
+        assertThat(browser.getText("xpath=//*[@id=\"transactionTable_data\"]/tr[1]/td[7]/div"), is(balance.toString()));
+
+    }
+
+    private void doTransaction() {
         browser.open(deploymentUrl.toString() + "/internal/transaction.xhtml?clear=clear&receiverId=" + RECEIVER.getId());
-        browser.type("id=transactionForm-amount", "15");
-        browser.type("id=transactionForm-subject", "Vielen Dank für die gute Arbeit");
+        browser.type("id=transactionForm-amount", String.valueOf(TRANSACTION_AMOUNT.intValue()));
+        browser.type("id=transactionForm-subject", SUBJECT);
         browser.click("id=transactionForm-showDialogButton");
         browser.click("id=transactionForm-confirm");
         browser.waitForPageToLoad("15000");
+    }
+
+    private void headerContainsNewAmount(BigDecimal expectedAmount) {
+        assertThat(browser.getText("xpath=//*[@id=\"loggedInHeaderForm-personalMenu_button\"]/span"), is("Kontostand: " + expectedAmount));
     }
 
     @Singleton

6412297db6d692a2c7a4fd1f5f0f2203e89c76c2
Niels Falk <niels@elbtrial.com>
2013-05-15 02:54:08 +0200

    try fixing joda with postgres

--- src/main/java/de/hh/changeRing/infrastructure/eclipselink/MappingCustomizer.java
+++ src/main/java/de/hh/changeRing/infrastructure/eclipselink/MappingCustomizer.java
@@ -30,7 +30,7 @@ import org.eclipse.persistence.mappings.converters.Converter;
 import org.joda.time.*;
 
 import java.lang.reflect.Field;
-import java.util.Date;
+import java.sql.Timestamp;
 import java.util.Map;
 import java.util.concurrent.ConcurrentMap;
 import java.util.logging.Logger;
@@ -76,7 +76,7 @@ public class MappingCustomizer implements DescriptorCustomizer {
             Field field = Reflection.forClass(clazz).findField(attribName);
             if (converters.containsKey(field.getType())) {
                 dtfMapping.setConverter(converters.get(field.getType()));
-                dtfMapping.setFieldClassification(Date.class);
+                dtfMapping.setFieldClassification(Timestamp.class);
             }
         }
     }
